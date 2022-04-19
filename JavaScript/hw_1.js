df6fa057a90ed3df3087d2fe7147393e3d6282b5
ZaytcevaYulia <j.zaytceva@mail.ru>
2022-04-19 10:33:01 +0300

    hw

--- JavaScript/hw_1.js
+++ JavaScript/hw_1.js
@@ -23,7 +23,7 @@ let item_7 = String(item_6);
 let item_7_type = typeof(item_7);
 console.log(`item_7 == ${item_7}, item_6_type == ${item_7_type}`);
 
-let age_1 = 10;
+let age_1 = 60;
 let age_2 = 18;
 let age_3 = 60;
 
--- JavaScript/hw_1_2.js
+++ JavaScript/hw_1_2.js
@@ -52,24 +52,3 @@ const checkAgeChange = function(age){
     }
 }    
 checkAgeChange("22");
-
-//Задание 4***
-const checkAgePrompt = function(age){
-    if (Number(age)){
-        if (age < age_2) {
-            alert("You don't have access cause your age is " + age + " It's less than")
-        } else if (age >= age_2 && age < age_3) {
-            alert("Welcome!")
-        } else if (age > age_3) {
-            alert("Keep calm and look Culture channel")
-        } else {
-            alert("Technical work")
-        }
-    } else {
-        alert("You have a mistake!")
-    }
-}    
-let userAge = prompt("Enter user's age");
-checkAgePrompt(userAge);
-
-
