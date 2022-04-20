c3b606922de8517f54611ebce83c61888bf8ea08
ZaytcevaYulia <100634732+ZaytcevaYulia@users.noreply.github.com>
2022-04-20 23:54:49 +0300

    hw

--- JavaScript/hw_1_2.js
+++ JavaScript/hw_1_2.js
@@ -3,7 +3,7 @@ let age_2 = 18;
 let age_3 = 60;
 const checkAge = function(age){
     if (age < age_2) {
-        console.log("You don't have access cause your age is " + age + " It's less than")
+        console.log("You don't have access cause your age is " + age + " It's less then 18")
     } else if (age >= age_2 && age < age_3) {
         console.log("Welcome!")
     } else if (age > age_3) {
