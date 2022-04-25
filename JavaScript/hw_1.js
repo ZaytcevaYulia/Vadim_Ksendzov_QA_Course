455a08b8a679de9173369f817e18a40d88ee3c3b
ZaytcevaYulia <j.zaytceva@mail.ru>
2022-04-25 22:53:59 +0300

    hw

--- JavaScript/hw_1.js
+++ JavaScript/hw_1.js
@@ -40,10 +40,22 @@ let item_6 = 15;
 let item_6_type = typeof(item_6);
 console.log("item_6 == " + item_6, "item_6_type == " + item_6_type);
 
+// 22. Создать переменную item_7 и в ней преобразовать item_6 в String.
+// 23. Создать переменную item_7_type
+// 24. Присвоить переменной item_7_type тип переменной item_7
+// 25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
 let item_7 = String(item_6);
 let item_7_type = typeof(item_7);
 console.log(`item_7 == ${item_7}, item_6_type == ${item_7_type}`);
 
+// 26. Создать переменную “age_1” и присвоить ей значение 10
+// 27. Создать переменную “age_2” и присвоить ей значение 18
+// 28. Создать переменную “age_3” и присвоить ей значение 60
+// 29. Создать if в котором будите проверять значение переменной age_1
+// 30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
+// 31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
+// 32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
+// 33. Иначе выводите “Technical work”.
 let age_1 = 10;
 let age_2 = 18;
 let age_3 = 60;
